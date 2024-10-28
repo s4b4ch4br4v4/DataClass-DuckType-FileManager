152a673d9b62eca03f74f3b3e0adcec716f3f8bb
Saba <sabachabrava2004@mail.ru>
2024-10-28 23:20:56 +0400

    Added docs and yes gitignore

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
 def main():
+    """Range is an iterator. Very interesting..."""
     my_range = range(3).__iter__()
     print(my_range.__next__())
     print(my_range.__next__())
     print(my_range.__next__())
 
+    """A simple algorithm for finding perfect numbers."""
     for n in range(2, 1000):
         divisors = []
         for x in range(1, n):
